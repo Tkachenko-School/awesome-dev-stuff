41b90f65f51bcd24a15917e2af610ad971d6c74b
Vadim <vadim.putrov@gmail.com>
2019-03-24 20:54:12 +0200

    change siteConfig

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,24 +21,24 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  title: 'Awesome dev stuff', // Title for your website.
+  tagline: 'Awesome-dev-stuff',
+  url: 'https://groceristar.github.io', // Your website URL
+  baseUrl: '/awesome-dev-stuff/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'awesome-dev-stuff',
+  organizationName: 'groceristar',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'summary', label: 'summary'},
+    {doc: 'summary', label: 'Summary'},
     {doc: 'doc4', label: 'API'},
     {page: 'help', label: 'Help'},
   ],
