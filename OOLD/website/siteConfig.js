b1cbd8c9dc63daa01327b3d30b2d964e58aacad9
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-08-14 05:24:42 +0300

    123

--- OOLD/website/siteConfig.js
+++ OOLD/website/siteConfig.js
@@ -1,27 +1,3 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
-// See https://docusaurus.io/docs/site-config for all the possible
-// site configuration options.
-
-// List of projects/orgs using your project for the users page.
-const users = [
-  {
-    caption: 'User1',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/docusaurus.svg'.
-    image: '/img/docusaurus.svg',
-    infoLink: 'https://www.facebook.com',
-    pinned: true,
-  },
-];
-
-
-
 const siteConfig = {
   title: 'Awesome dev stuff', // Title for your website.
   tagline: 'Awesome-dev-stuff - website with links, related to coding',
@@ -41,7 +17,7 @@ const siteConfig = {
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     // { doc: 'summary', label: 'Summary' },
-    {doc: 'summary', label: 'Summary'}, 
+    {doc: 'summary', label: 'Summary'},
     {
       href: 'https://github.com/ChickenKyiv/awesome-dev-stuff',
        label: 'GitHub repository'
@@ -49,41 +25,12 @@ const siteConfig = {
     // {page: 'help', label: 'Help'},
   ],
 
-  // If you have users set above, you add it here:
-  users,
 
-  /* path to images for header/footer */
-  headerIcon: 'img/docusaurus.svg',
-  footerIcon: 'img/docusaurus.svg',
-  favicon: 'img/favicon.png',
-
-  /* Colors for website */
-  colors: {
-    primaryColor: '#2E8555',
-    secondaryColor: '#205C3B',
-  },
-
-  /* Custom fonts for website */
-  /*
-  fonts: {
-    myFont: [
-      "Times New Roman",
-      "Serif"
-    ],
-    myOtherFont: [
-      "-apple-system",
-      "system-ui"
-    ]
-  },
-  */
 
   // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} ChickenKyiv company`,
+  copyright: `Copyright © ${new Date().getFullYear()}
+  ChickenKyiv company`,
 
-  highlight: {
-    // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: 'default',
-  },
 
   // Add custom scripts here that would be placed in <script> tags.
   scripts: ['https://buttons.github.io/buttons.js'],
@@ -93,9 +40,6 @@ const siteConfig = {
   // No .html extensions for paths.
   cleanUrl: true,
 
-  // Open Graph and Twitter card images.
-  ogImage: 'img/docusaurus.png',
-  twitterImage: 'img/docusaurus.png',
 
   // Show documentation's last contributor's name.
   // enableUpdateBy: true,
@@ -107,5 +51,3 @@ const siteConfig = {
   // template. For example, if you need your repo's URL...
   repoUrl: 'https://github.com/ChickenKyiv/awesome-dev-stuff',
 };
-
-module.exports = siteConfig;
