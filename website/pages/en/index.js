139aed1f8891c73a03ca33e704e8004331621444
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-08-14 06:28:11 +0300

    111

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -76,6 +76,184 @@ class HomeSplash extends React.Component {
   }
 }
 
+
+const MainPage = props => (
+  <div
+    className="showcaseSection paddingBottom"
+    style={{ textAlign: 'center' }}>
+    <div className="tableofcontents" style={{ textAlign: 'center' }}>
+
+      <p>
+        Welcome to awesome-dev-stuff website(yeah, I should find a better name).
+      </p>
+      <p>
+        It's a place, where our team keeping different articles,
+      </p>
+      <p>
+       related to software development.
+      </p>
+      <p>
+        We sharing those links frequently with beginners, interns, juniors
+      </p>
+      <p>
+      and you may like it interesting as well.
+      </p>
+      <p>
+        There a lot of cool information, sorted by different topics.
+      </p>
+    </div>
+  </div>
+);
+
+
+// const TryOut = () => (
+//   <Block id="try">
+//     {[
+//       {
+//         content: 'Perfect your React skills!',
+//         image: `https://raw.githubusercontent.com/GroceriStar/creative/master/website-illustrations/moving_forward.svg?sanitize=true`,
+//         imageAlign: 'left',
+//         title: 'React Best practices',
+//       },
+//     ]}
+//   </Block>
+// );
+
+// const Description = () => (
+//   <Block background="white">
+//     {[
+//       {
+//         content: 'How to find tech job or land an internship in tech company?',
+//         image: `https://raw.githubusercontent.com/GroceriStar/creative/master/website-illustrations/astronaut.svg?sanitize=true`,
+//         imageAlign: 'right',
+//         title:  'How to find coding job',
+//       },
+//     ]}
+//   </Block>
+// );
+
+// const Description2 = () => (
+//   <Block background="white">
+//     {[
+//       {
+//         content: 'Just want to read hand-picked, interesting stories?',
+//         image: `https://raw.githubusercontent.com/GroceriStar/creative/master/website-illustrations/wireframe.svg?sanitize=true`,
+//         imageAlign: 'left',
+//         title:  'Handpicked stories',
+//       },
+//     ]}
+//   </Block>
+// );
+
+// const Description3 = () => (
+//   <Block background="white">
+//     {[
+//       {
+//         content: 'Different stories to read - give them a chance!',
+//         image: `https://raw.githubusercontent.com/GroceriStar/creative/master/website-illustrations/mind_map.svg?sanitize=true`,
+//         imageAlign: 'right',
+//         title: 'Different topics like GraphQL, Open Source movement, CSS, Design,  and many more',
+//       },
+//     ]}
+//   </Block>
+// );
+
+
+// <TryOut />
+// <Description />
+// <Description2 />
+// <Description3 />
+//
+// <TableOfContents />
+// image: `https://raw.githubusercontent.com/GroceriStar/creative/master/website-illustrations/moving_forward.svg?sanitize=true`,
+
+
+// https://raw.githubusercontent.com/GroceriStar/creative/master/illustrations/Vegetables_Tomato%20(1).png
+
+// @TODO will deploy break this hardcoded links?
+// I think yes
+const TableOfContents = props => (
+  <div
+    className="showcaseSection paddingBottom"
+    style={{ textAlign: 'center' }}>
+
+
+    <h2>Table of contents</h2>
+    <div className="tableofcontents" >
+
+    <ul className="border-bottom-3px">
+
+      <li>
+        <a href="/awesome-dev-stuff/docs/jobs/jobs-section">Jobs index</a>
+      </li>
+        <li><a href="/awesome-dev-stuff/docs/jobs/tech-job">Startup Jobs</a></li>
+        <li><a href="/awesome-dev-stuff/docs/jobs/internships">Internships</a></li>
+        <li><a href="/awesome-dev-stuff/docs/jobs/job-boards">Tech Job Boards</a></li>
+        <li><a href="/awesome-dev-stuff/docs/jobs/jobs">Remote Jobs</a></li>
+    </ul>
+
+    <ul class="border-bottom-3px">
+
+        <li>
+        <a href="/awesome-dev-stuff/docs/create-npm-module/create-npm-module-section">
+        Publishing your module at NPM index</a></li>
+          <li><a href="/awesome-dev-stuff/docs/create-npm-module/create-npm-module">First articles to read</a></li>
+          <li><a href="/awesome-dev-stuff/docs/create-npm-module/create-npm-module-babel">Connect Babel to your project</a></li>
+          <li><a href="/awesome-dev-stuff/docs/create-npm-module/create-npm-module-rollup">RollupJS</a></li>
+          <li><a href="/awesome-dev-stuff/docs/create-npm-module/create-npm-module-rollup-react">Rollup + React configuration</a></li>
+    </ul>
+
+    <ul class="border-bottom-3px">
+      <li><a href="/awesome-dev-stuff/docs/js/js-section">JavaScript index</a></li>
+        <li><a href="/awesome-dev-stuff/docs/js/js">JS articles</a></li>
+        <li><a href="/awesome-dev-stuff/docs/js/complex-structures">Complex structures</a></li>
+        <li><a href="/awesome-dev-stuff/docs/js/nodejs-corb-cors">NodeJS CORB CORS</a></li>
+      </ul>
+
+    <ul class="border-bottom-3px">
+        <li><a href="/awesome-dev-stuff/docs/react/react-section">React index</a></li>
+          <li><a href="/awesome-dev-stuff/docs/react/awesome-react-book">Awesome React Book by Flavio Copes</a></li>
+          <li><a href="/awesome-dev-stuff/docs/react/react">Different articles related to React ecosystem</a></li>
+          <li><a href="/awesome-dev-stuff/docs/react/react-children">React Children components</a></li>
+          <li><a href="/awesome-dev-stuff/docs/react/styling">Styled Components</a></li>
+          <li><a href="/awesome-dev-stuff/docs/react/react-best-practices">React best practices</a></li>
+          <li><a href="/awesome-dev-stuff/docs/react/project-structure">React project structure</a></li>
+    </ul>
+
+    <ul class="border-bottom-3px">
+    <li><a href="/awesome-dev-stuff/docs/css/ss-section">CSS index</a></li>
+      <li><a href="/awesome-dev-stuff/docs/css/css">CSS</a></li>
+      <li><a href="/awesome-dev-stuff/docs/css/css-grid">CSS Grid</a></li>
+      <li><a href="/awesome-dev-stuff/docs/css/flexbox">Flexbox</a></li>
+      </ul>
+
+    <ul class="border-bottom-3px">
+      <li>
+      <a href="/awesome-dev-stuff/docs/stories/stories">
+        Stories for developers</a>
+        </li>
+    </ul>
+
+    <ul class="border-bottom-3px">
+        <li><a href="/awesome-dev-stuff/docs/unsorted/unsorted-section">Unsorted content</a></li>
+        <li><a href="/awesome-dev-stuff/docs/courses">Free code courses</a></li>
+        <li><a href="/awesome-dev-stuff/docs/open-source/open-source">Open source</a></li>
+        <li><a href="/awesome-dev-stuff/docs/product-hunt/launch-producthunt">Launch your project on ProductHunt Articles Collection</a></li>
+
+        <li><a href="/awesome-dev-stuff/docs/machine-learning/machine-learning">Machine learning</a></li>
+        <li><a href="/awesome-dev-stuff/docs/design/design">Design</a></li>
+        <li><a href="/awesome-dev-stuff/docs/git/git">Mastering Git</a></li>
+        <li><a href="/awesome-dev-stuff/docs/github/cool-github-repositories">Cool Github Repositories</a></li>
+        <li><a href="/awesome-dev-stuff/docs/graphql/graphql">GraphQL</a></li>
+        <li><a href="/awesome-dev-stuff/docs/indie-hackers/indie-hackers">IndieHackers articles/podcasts collection</a></li>
+        <li><a href="/awesome-dev-stuff/docs/images">Credits: Images</a></li>
+    </ul>
+
+
+    </div>
+  </div>
+);
+
 class Index extends React.Component {
   render() {
     const {config: siteConfig, language = ''} = this.props;
@@ -220,6 +398,8 @@ class Index extends React.Component {
             <LearnHow />
             <TryOut />
             <Description />
+
+            {/* <TableOfContents /> */}
             {/*<Showcase /> */}
           </div>
 
